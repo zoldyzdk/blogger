6386efacdeca321392061046b5fd8ad846d51677
Hewerton Zoldy <hewerton.soares@yahoo.com>
2024-10-09 19:51:38 -0300

    feat(conf): add config to content collection

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+  })
+})
+
+export const collections = {
+  'blog': blogCollection,
+}
